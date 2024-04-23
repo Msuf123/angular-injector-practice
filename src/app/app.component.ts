cc959967ecee116626fd34ea624fa43cd2cbd6e8
Msuf123 <akshatmalik18t@gmail.com>
2024-04-23 22:19:43 +0530

    Understood @Injectable and injecting a service into another

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { MathsService } from './services/maths/maths.service';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { token } from './app.config';
+import { LoggerOperationsService } from './services/logger-service/logger-operations.service';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ import { token } from './app.config';
 export class AppComponent {
   formBuilder=inject(FormBuilder)
   state=inject(token)
-  constructor(@Inject('Maths_Service') private maths:MathsService){
+  constructor(private maths:MathsService){
     console.log(`This is a current state ${this.state}`)
   }
   inputAdd=this.formBuilder.group({
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { MathsService } from './services/maths/maths.service';
 import { BetterMathsService } from './services/better-maths/better-maths.service';
+import { LoggerOperationsService } from './services/logger-service/logger-operations.service';
 export const token=new InjectionToken<number>('I will show number')
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),{provide:'Maths_Service',useClass:BetterMathsService},{provide:token,useValue:1}]
+  providers: [provideRouter(routes),{provide:MathsService,useClass:BetterMathsService},LoggerOperationsService,{provide:token,useValue:1}]
 };
